a4e7aba0244926cce390a6700527994e67b97bd1
Jessa Mae <143053099+resmajessa@users.noreply.github.com>
2023-12-19 01:12:28 +0800

    Profiles.js

---
+++ Profiles.js
@@ -0,0 +1,112 @@
+export const credentials =[
+    {
+		"ID": 1,
+		"Firstname": "Merritt Michael",
+		"Lastname": "Camden Baxter",
+		"Course": "BSIT",
+		"Year": 1,
+		"Section": "D",
+		"Usertype": "Teacher",
+		"Username": "ligula. Aenean gravida",
+		"Password": "Aenean gravida"
+	},
+	{
+		"ID": 2,
+		"Firstname": "Inez Sherman",
+		"Lastname": "Quinlan Estrada",
+		"Course": "BSIT",
+		"Year": 1,
+		"Section": "A",
+		"Usertype": "Student",
+		"Username": "vestibulum.",
+		"Password": "In tincidunt"
+	},
+	{
+		"ID": 3,
+		"Firstname": "Mollie Nunez",
+		"Lastname": "Grant Russo",
+		"Course": "BSED",
+		"Year": 3,
+		"Section": "A",
+		"Usertype": "Student",
+		"Username": "placerat,",
+		"Password": "amet"
+	},
+	{
+		"ID": 4,
+		"Firstname": "Megan Pace",
+		"Lastname": "Marshall Pearson",
+		"Course": "BSCS",
+		"Year": 2,
+		"Section": "B",
+		"Usertype": "Teacher",
+		"Username": "eleifend,",
+		"Password": "leo elementum sem,"
+	},
+	{
+		"ID": 5,
+		"Firstname": "Bianca Diaz",
+		"Lastname": "Oren Roach",
+		"Course": "BSIT",
+		"Year": 2,
+		"Section": "D",
+		"Usertype": "Student",
+		"Username": "mauris eu",
+		"Password": "Sed nunc est,"
+	},
+	{
+		"ID": 6,
+		"Firstname": "Dustin Cooley",
+		"Lastname": "Nolan Munoz",
+		"Course": "BSIT",
+		"Year": 1,
+		"Section": "D",
+		"Usertype": "Student",
+		"Username": "erat. Sed",
+		"Password": "adipiscing ligula. Aenean"
+	},
+	{
+		"ID": 7,
+		"Firstname": "Molly Holden",
+		"Lastname": "Armando Berger",
+		"Course": "BSIT",
+		"Year": 3,
+		"Section": "A",
+		"Usertype": "Teacher",
+		"Username": "eget magna.",
+		"Password": "Sed eget lacus."
+	},
+	{
+		"ID": 8,
+		"Firstname": "Colt Bright",
+		"Lastname": "Declan Peterson",
+		"Course": "BSED",
+		"Year": 2,
+		"Section": "B",
+		"Usertype": "Student",
+		"Username": "elit elit",
+		"Password": "enim. Nunc ut"
+	},
+	{
+		"ID": 9,
+		"Firstname": "Heidi West",
+		"Lastname": "Dolan Stewart",
+		"Course": "BSIT",
+		"Year": 4,
+		"Section": "B",
+		"Usertype": "Teacher",
+		"Username": "nec, cursus",
+		"Password": "Duis sit"
+	},
+	{
+		"ID": 10,
+		"Firstname": "Maisie Frost",
+		"Lastname": "Kiona England",
+		"Course": "BSIT",
+		"Year": 1,
+		"Section": "C",
+		"Usertype": "Student",
+		"Username": "ante",
+		"Password": "ornare placerat,"
+	},
+]
